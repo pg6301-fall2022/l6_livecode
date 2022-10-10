6bbd249ab7cda3dc820eb89971146d7d1f131ee1
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-10-10 10:17:07 +0200

    Step 6 - A bit of React

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.post("/login", (req, res, next) =>{
     res.sendStatus(401);
 });
 
-app.use(express.static("public"));
+app.use(express.static("../client/dist"));
 
 const server = app.listen(process.env.PORT || 3000,
     () => {
